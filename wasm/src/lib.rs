37f35588ab85765f3430175a0984362173fd31b4
yunix <>
2023-12-17 11:26:45 +0900

    :+1: yukicoder score contest002のビジュアライザを実装

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,8 +1,9 @@
 use wasm_bindgen::prelude::*;
+mod util;
 
 #[wasm_bindgen]
 pub fn gen(seed: i32) -> String {
-    "".to_string()
+    util::gen(seed as u64).to_string()
 }
 
 #[wasm_bindgen(getter_with_clone)]
@@ -14,14 +15,19 @@ pub struct Ret {
 
 #[wasm_bindgen]
 pub fn vis(_input: String, _output: String, turn: usize) -> Ret {
+    let input = util::parse_input(&_input);
+    let output = util::parse_output(&_output, input.m);
+    let (score, err, svg) = util::vis(&input, &output, turn as usize);
     Ret {
-        score: 0,
-        err: "".to_string(),
-        svg: "".to_string(),
+        score: score,
+        err: err.to_string(),
+        svg: svg.to_string(),
     }
 }
 
 #[wasm_bindgen]
 pub fn get_max_turn(_input: String, _output: String) -> usize {
-    0
+    let input = util::parse_input(&_input);
+    let output = util::parse_output(&_output, input.m);
+    (output.v - 1) as usize
 }
