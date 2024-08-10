a882710d1ee97d54c82095d7202166ad888fc437
hari64 <hari64boli64@gmail.com>
2024-08-10 16:36:45 +0900

    fix router settings

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,7 +3,6 @@ import type { FC } from 'react';
 import { Navigate, Route, Routes, useLocation } from 'react-router-dom';
 
 import InitWasm from 'components/InitWasm';
-import Home from 'components/templates/Home';
 
 const IndexRoutes: FC = () => {
   const { hash, pathname } = useLocation();
@@ -16,10 +15,7 @@ const IndexRoutes: FC = () => {
 
   return (
     <Routes>
-      <Route path="hoge">
-        <Route path="" element={<InitWasm />} />
-      </Route>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<InitWasm />} />
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
